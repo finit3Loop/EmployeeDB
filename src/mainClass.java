626aa61da8c2b4266817d3b0edee18d8b6236fa3
finit3Loop <37601914+finit3Loop@users.noreply.github.com>
2019-01-27 00:53:53 -0600

    Building

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,5 +1,4 @@
 
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,27 +7,33 @@ import javax.swing.JFileChooser;
 public class mainClass {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("pick the vector file");
 		JFileChooser c = new JFileChooser();
 		c.showOpenDialog(c);
-		
+
 		ArrayList<Employee> Company = new ArrayList<Employee>();
 		int choice;
 		do {
 			System.out.println("What would you like to do? :");
 			Scanner scan = new Scanner(System.in);
 			choice = scan.nextInt();
-		switch (choice) { 
+			switch (choice) {
+			// add Employee
+			case 1:
+				Employee cal = new Employee();
+				Company.add(cal);
+				break;
 			default:
-					break;
-		}
-		}while(choice != 0);
-		Employee cal = new Employee();
-		
-		Company.add(cal);
-	}
+				break;
+			}
 
-	
+			//scan.close();
+		} while (choice != 0);
+		Employee cal = new Employee();
+
+		Company.add(cal);
+		System.out.println(Company.toString());
+	}
 
 }
