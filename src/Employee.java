cd1d268c75231fe06f351b729b3a807600c58e70
finit3Loop <37601914+finit3Loop@users.noreply.github.com>
2019-01-28 00:44:49 -0600

    Updates

--- src/Employee.java
+++ src/Employee.java
@@ -1,24 +1,32 @@
 
-
 import java.util.*;
 
 public class Employee {
 	
+	private String name;
 	private int employeeNumber;
 	private int salary;
 	private int daysOff;
 	private int beginingTime, endTime;
-	
 
-	public Employee()
-	{
+	public Employee() {
 		Random rand = new Random();
 		
+		this.name = "Steve";
 		this.employeeNumber = rand.nextInt();
 		this.salary = 50000;
-		this.daysOff = 0 ;
+		this.daysOff = 0;
 		this.beginingTime = 900;
 		this.endTime = 1700;
 	}
-	
+
+	public String toString() {
+
+		String retString = new String();
+		retString = this.name;
+		retString += ", "+this.employeeNumber;
+		
+		return retString;
+	}
+
 }
--- src/mainClass.java
+++ src/mainClass.java
@@ -1,6 +1,5 @@
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 import javax.swing.JFileChooser;
 
@@ -8,27 +7,35 @@ public class mainClass {
 
 	public static void main(String[] args) {
 
-		System.out.println("pick the vector file");
-		JFileChooser c = new JFileChooser();
-		c.showOpenDialog(c);
+		/*
+		 * System.out.println("pick the vector file"); JFileChooser c = new
+		 * JFileChooser(); c.showOpenDialog(c);
+		 */
 
 		ArrayList<Employee> Company = new ArrayList<Employee>();
 		int choice;
 		do {
 			System.out.println("What would you like to do? :");
 			Scanner scan = new Scanner(System.in);
-			choice = scan.nextInt();
+			try {
+				choice = scan.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("Not a number");
+				choice = 2;
+			}
 			switch (choice) {
 			// add Employee
 			case 1:
 				Employee cal = new Employee();
 				Company.add(cal);
 				break;
+			case 2:
+				break;
 			default:
 				break;
 			}
 
-			//scan.close();
+			// scan.close();
 		} while (choice != 0);
 		Employee cal = new Employee();
 
